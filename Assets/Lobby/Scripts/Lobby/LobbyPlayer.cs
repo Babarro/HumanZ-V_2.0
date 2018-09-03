1986b93b1e0d6e799887abf6f3ef381875fd0d04
jaimehuesa <j.huesa@alumnos.upm.es>
2018-09-03 13:21:43 +0200

    Canvas lobby

--- Assets/Lobby/Scripts/Lobby/LobbyPlayer.cs
+++ Assets/Lobby/Scripts/Lobby/LobbyPlayer.cs
@@ -30,11 +30,11 @@ namespace Prototype.NetworkLobby
         [SyncVar(hook = "OnMyColor")]
         public Color playerColor = Color.white;
 
-        public Color OddRowColor = new Color(250.0f / 255.0f, 250.0f / 255.0f, 250.0f / 255.0f, 1.0f);
-        public Color EvenRowColor = new Color(180.0f / 255.0f, 180.0f / 255.0f, 180.0f / 255.0f, 1.0f);
+		public Color OddRowColor = new Color(0.0f, 0.0f, 0.0f, 0.7f); //new Color(250.0f / 255.0f, 250.0f / 255.0f, 250.0f / 255.0f, 1.0f);
+		public Color EvenRowColor = new Color(0.1f, 0.1f, 0.1f, 0.7f); //new Color(180.0f / 255.0f, 180.0f / 255.0f, 180.0f / 255.0f, 1.0f);
 
-        static Color JoinColor = new Color(255.0f/255.0f, 0.0f, 101.0f/255.0f,1.0f);
-        static Color NotReadyColor = new Color(34.0f / 255.0f, 44 / 255.0f, 55.0f / 255.0f, 1.0f);
+		static Color JoinColor = new Color(40.0f/255.0f, 54.0f/255.0f , 34.0f/255.0f,1.0f); //new Color(255.0f/255.0f, 0.0f, 101.0f/255.0f,1.0f);
+		static Color NotReadyColor = new Color(40.0f/255.0f, 54.0f/255.0f , 34.0f/255.0f,1.0f); //new Color(34.0f / 255.0f, 44 / 255.0f, 55.0f / 255.0f, 1.0f);
         static Color ReadyColor = new Color(0.0f, 204.0f / 255.0f, 204.0f / 255.0f, 1.0f);
         static Color TransparentColor = new Color(0, 0, 0, 0);
 
@@ -192,7 +192,7 @@ namespace Prototype.NetworkLobby
         public void OnMyColor(Color newColor)
         {
             playerColor = newColor;
-            colorButton.GetComponent<Image>().color = newColor;
+            //colorButton.GetComponent<Image>().color = newColor;
         }
 
         //===== UI Handler
