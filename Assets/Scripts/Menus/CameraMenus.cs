ad5c78600f6ebf04875d87581df279ca475f0c10
jaimehuesa <j.huesa@alumnos.upm.es>
2018-09-06 18:01:56 +0200

    Canvas Lobby

---
+++ Assets/Scripts/Menus/CameraMenus.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMenus : MonoBehaviour {
+
+	public Camera CameraLobby;
+	public Camera CameraServers;
+
+	// Use this for initialization
+	void Awake() {
+		SetCamerasRef ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetCamerasRef(){
+		CameraLobby = GameObject.FindGameObjectWithTag ("CameraLobby").GetComponent<Camera>();
+		CameraServers = GameObject.FindGameObjectWithTag ("CameraServer").GetComponent<Camera>();
+
+		if (CameraLobby == null || CameraServers == null)
+			Debug.Log ("No se ha obtenido referencia");
+	}
+
+}
---
+++ Assets/Scripts/Menus/SetQuitCameras.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetQuitCameras : MonoBehaviour {
+
+	public CameraMenus cameraMenus;
+
+	void OnEnable(){
+
+
+		if (gameObject.name == "MainPanel") {
+			cameraMenus.CameraLobby.gameObject.SetActive (true);
+			cameraMenus.CameraServers.gameObject.SetActive (false);
+		} else if (gameObject.name == "ServerListPanel") {
+			cameraMenus.CameraLobby.gameObject.SetActive (false);
+			cameraMenus.CameraServers.gameObject.SetActive (true);
+		}
+
+	}
+}
